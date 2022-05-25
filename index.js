67e4297c1e9e3868d3b4cf7db3962f2fe8f580da
mkaychuks <sachufusi@gmail.com>
2022-05-25 20:06:55 +0100

    Initial commit :smile:

---
+++ index.js
@@ -0,0 +1,12 @@
+let text = "Please visit the nearest Microsoft store"
+
+let newText = text.replace("store", 'Shop')
+
+console.log(text.split(" ")) // returns ['Please', 'visit', 'the', 'nearest', 'Microsoft', 'store']
+console.log(text) // returns Please visit the nearest Microsoft store
+console.log(newText) // returns Please visit the nearest Microsoft Shop
+
+
+
+
+console.log(text.search('e'))
