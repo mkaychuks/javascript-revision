44b338a5af8a386422910827bcfff8c524d0a48e
mkaychuks <sachufusi@gmail.com>
2022-05-25 20:17:42 +0100

    String includes()

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // index of a string (first occurence of an text in a string)
 
 let str = "Please locate where 'the' is located";
-
 console.log(str.indexOf('the'))
+
+
+// search a string -> returns the 'true' if a string contains a specified value
+console.log(str.includes('locate'))
