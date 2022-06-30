e41e18ee425bca3540798fa0e6d3655adff6870f
mkaychuks <sachufusi@gmail.com>
2022-06-30 16:16:10 +0100

    trying out some string methods in typescript

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,9 @@
 "use strict";
 let age = 2;
-let course = "Typescript";
+let course = "Type script";
 let is_published = false;
 let courses = course.endsWith("p");
-console.log(typeof (courses));
+console.log(course.toUpperCase());
+console.log(course.length);
+console.log(course.slice(3, 12));
+console.log(course.trim());
