1edd5038d9040fb5866d24b5c91b23b362421980
mkaychuks <sachufusi@gmail.com>
2022-06-30 16:16:29 +0100

    trying out some string methods in typescript

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 // built in types in typescript
 
 let age: number = 2
-let course: string= "Typescript"
+let course: string= "Type script"
 let is_published: boolean = false
 
-
-let courses = course.endsWith("p") // trying out some string methods in typescript
-
-console.log(typeof(courses))
+// trying out some string methods in typescript
+let courses = course.endsWith("p") 
+console.log(course.toUpperCase())
+console.log(course.length)
+console.log(course.slice(3, 12))
+console.log(course.trim())
